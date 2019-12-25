89bdab2adde79c5c34fb258bc8c1e02fd5df1736
tahacan.atak <tahacan.atak@gmail.com>
2019-12-25 17:11:47 +0300

    add ajax basket

--- EShop.Web/Controllers/BasketController.cs
+++ EShop.Web/Controllers/BasketController.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using EShop.Web.Interfaces;
 using EShop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EShop.Web.Controllers
 {
@@ -23,17 +30,40 @@ namespace EShop.Web.Controllers
         }
 
         [HttpPost]
-        public  IActionResult AddToBasket(int productId, int quantity = 1)
+        public async Task<IActionResult> AddToBasket(int productId, int quantity = 1)
         {
             _basketViewModelService.AddToBasket(productId, quantity);
 
             var vm = new AjaxHeaderBasketViewModel
             {
-                HeaderBasketHtml = "sfsdfsfsdf",
+                HeaderBasketHtml = await RenderViewComponentAsync("HeaderCart", null),
                 TotalItems = _basketViewModelService.TotalItems()
             };
 
-            return Json(vm); //toplam öge sayısını ve sepet html'i dondur
+            return Json(vm); // toplam öğe sayısını ve sepet html'ini döndür
+        }
+
+        // https://gist.github.com/pauldotknopf/b424e9b8b03d31d67f3cce59f09ab17f
+        public async Task<string> RenderViewComponentAsync(string viewComponent, object args = null)
+        {
+            var sp = HttpContext.RequestServices;
+
+            var helper = new DefaultViewComponentHelper(
+                sp.GetRequiredService<IViewComponentDescriptorCollectionProvider>(),
+                HtmlEncoder.Default,
+                sp.GetRequiredService<IViewComponentSelector>(),
+                sp.GetRequiredService<IViewComponentInvokerFactory>(),
+                sp.GetRequiredService<IViewBufferScope>());
+
+            using (var writer = new StringWriter())
+            {
+                var context = new ViewContext(ControllerContext, NullView.Instance, ViewData, TempData, writer, new HtmlHelperOptions());
+                helper.Contextualize(context);
+                var result = await helper.InvokeAsync(viewComponent, args);
+                result.WriteTo(writer, HtmlEncoder.Default);
+                await writer.FlushAsync();
+                return writer.ToString();
+            }
         }
     }
 }
